1d3fa199dac8c4e4af408e1a494ed20865d66451
deonjoshy <deonjoshy@gmail.com>
2024-03-23 22:44:48 +0530

    added logo and name

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,8 +4,10 @@ import './Navbar.css'
 export const Navbar = () => {
   return (
     <div className='navbar'>
-        <div className="nav-logo">logo</div>
-        <div className="nav-name">shop</div>
+        <div className="nav-logo">
+          <img src = '/logo_final.jpg' alt ='not found' />
+        </div>
+        <div className="nav-name">MattressMattersOshawa</div>
     </div>
   )
 }
